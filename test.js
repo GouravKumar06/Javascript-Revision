5dbfa8106f32f2a93d7254b7a26ee0193851290a
Gourav kumar <121687249+GouravKumar06@users.noreply.github.com>
2024-03-29 19:56:48 +0000

    3rd changes

--- test.js
+++ test.js
@@ -1,2 +1,8 @@
-console.log("hello gourav");
-console.log("my name is Gourav ");
+const accountNumber = 399;
+let accountEmail = "gka98963@gmail.com";
+var accountType = "saving";
+accountPass = true
+
+accountPass = false
+
+console.table([accountNumber,accountEmail,accountType,accountPass]);
