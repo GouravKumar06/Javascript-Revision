71ad64630439c509f20ff6c55af22421781595fa
Gourav kumar <121687249+GouravKumar06@users.noreply.github.com>
2024-03-30 06:10:32 +0000

    conversion

--- README.md
+++ README.md
@@ -2,15 +2,15 @@
 
 /*
     Datatypes in javacript :-
-        1. Primitive -> Numbers
-                        String 
-                        Boolean
-                        Undefined
-                        Null
+        1. Primitive -> Numbers,
+                        String, 
+                        Boolean,
+                        Undefined,
+                        Null (typeof null is Object)
                         Symbol -> used for uniqueness
         
         2.Non-primitive -> Arrays  (this is also an object in javascript)
                            Function (this is also an object in javascript)
                            Object 
-                           
+
 */
---
+++ Type_Conversion/test.js
@@ -0,0 +1,50 @@
+let score = "33"
+let value = "44abdcs"
+let first = null
+let second = undefined
+
+console.log(typeof score)
+console.log(typeof first)
+console.log(typeof second)
+
+let numberScore = Number(score) // used to change string into number
+let numberValue = Number(value)
+let numberFirst = Number(first)
+let numberSecond = Number(second)
+
+console.log(typeof numberScore)    
+console.log(typeof numberValue)
+console.log(typeof numberFirst)
+console.log(typeof numberSecond)
+
+console.log(numberValue)    // NaN
+console.log(numberFirst)    // 0
+console.log(numberSecond)   // Nan
+
+/*Summmary
+  1. "33" -> 33 (type -> Number)
+  2. "33abcfd" -> NaN (type -> Number)
+  3. null -> 0 (type -> Number)
+  4. undefined -> 0   (type -> Number)
+  5. true -> 1 (type -> Number)
+  6. false -> 0 (type -> Number)
+
+*/
+
+
+//2. Boolean conversion
+let loggedIn = undefined
+let booleanLoggedIn = Boolean(loggedIn)
+
+console.log(typeof booleanLoggedIn)
+console.log(booleanLoggedIn)
+
+/*Summmary
+  1. "33" -> true (type -> Boolean)
+  2. "" -> false (type -> Boolean)
+  3. null -> fasle (type -> Boolean)
+  4. undefined -> false   (type -> Boolean)
+  5. 0 -> false (type -> Boolean)
+  6. 1 -> true (type -> Boolean)
+
+*/
